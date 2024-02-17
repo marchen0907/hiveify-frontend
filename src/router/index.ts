a1f7173b6e08d693ef93ecd3b3e020153c144e54
wujunyi0907 <wujunyi0907@163.com>
2024-02-17 12:56:28 +0800

    Refactor 项目结构优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 
 import { lazy } from "react";
 
-const NotFound = lazy(() => import("@/components/error/NotFound"));
-const UserLogin = lazy(() => import("@/components/login/UserLogin"));
-const PageHome = lazy(() => import("@/components/home/PageHome"));
+const NotFound = lazy(() => import("@/page/error/NotFound"));
+const UserLogin = lazy(() => import("@/page/login/UserLogin"));
+const PageHome = lazy(() => import("@/page/home/PageHome"));
 
 const router = createBrowserRouter([
   {
