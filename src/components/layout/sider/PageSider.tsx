e2b0de465f9b645e24747a66098ead150c1e7c59
wujunyi0907 <wujunyi0907@163.com>
2024-03-03 16:01:31 +0800

    Feat 新增子侧栏内容及样式

--- src/components/layout/sider/PageSider.tsx
+++ src/components/layout/sider/PageSider.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
 
-import { Layout, Avatar, Dropdown, Button } from "antd";
+import { Layout, Avatar, Dropdown, Button, Divider } from "antd";
 import type { MenuProps } from "antd";
 import { CloudSyncOutlined, UserOutlined } from "@ant-design/icons";
 import { Link, useLocation } from "react-router-dom";
@@ -9,7 +9,21 @@ import classnames from "classnames";
 
 import { useAppDispatch } from "@/store/hook";
 import { removeUser } from "@/store/modules/userSlice";
-import { Logo, Folder, Transmission, Setting } from "./SiderIcon";
+import {
+  LogoIcon,
+  FolderIcon,
+  TransmissionIcon,
+  SettingIcon,
+  ImageIcon,
+  DocumentIcon,
+  VideoIcon,
+  AudioIcon,
+  OtherIcon,
+  UploadIcon,
+  DownloadIcon,
+  SucceedIcon,
+  SyncIcon,
+} from "./SiderIcon";
 
 import "./PageSider.scss";
 
@@ -43,19 +57,134 @@ const PageSider: FC = () => {
     },
   ];
 
+  const GroupSub = () => {
+    return (
+      <>
+        <Link
+          to="/group/image"
+          className={classnames("sub-tab-item", {
+            "is-active": location.pathname === "/group/image",
+          })}
+        >
+          <ImageIcon />
+          <span className="sub-tab-item-text">图片</span>
+        </Link>
+        <Link
+          to="/group/document"
+          className={classnames("sub-tab-item", {
+            "is-active": location.pathname === "/group/document",
+          })}
+        >
+          <DocumentIcon />
+          <span className="sub-tab-item-text">文档</span>
+        </Link>
+        <Link
+          to="/group/video"
+          className={classnames("sub-tab-item", {
+            "is-active": location.pathname === "/group/video",
+          })}
+        >
+          <VideoIcon />
+          <span className="sub-tab-item-text">视频</span>
+        </Link>
+        <Link
+          to="/group/audio"
+          className={classnames("sub-tab-item", {
+            "is-active": location.pathname === "/group/audio",
+          })}
+        >
+          <AudioIcon />
+          <span className="sub-tab-item-text">音频</span>
+        </Link>
+        <Link
+          to="/group/other"
+          className={classnames("sub-tab-item", {
+            "is-active": location.pathname === "/group/other",
+          })}
+        >
+          <OtherIcon />
+          <span className="sub-tab-item-text">其他</span>
+        </Link>
+        <Divider />
+        <Link
+          to="/group/recycle"
+          className={classnames("sub-tab-item", {
+            "is-active": location.pathname === "/group/recycle",
+          })}
+        >
+          <span>回收站</span>
+        </Link>
+      </>
+    );
+  };
+
+  const TransmissionSub = () => {
+    return (
+      <>
+        <Link
+          to="/transmission/upload"
+          className={classnames("sub-tab-item", {
+            "is-active": location.pathname === "/transmission/upload",
+          })}
+        >
+          <UploadIcon />
+          <span className="sub-tab-item-text">上传</span>
+        </Link>
+        <Link
+          to="/transmission/upload"
+          className={classnames("sub-tab-item", {
+            "is-active": location.pathname === "/transmission/download",
+          })}
+        >
+          <DownloadIcon />
+          <span className="sub-tab-item-text">下载</span>
+        </Link>
+        <Link
+          to="/transmission/succeed"
+          className={classnames("sub-tab-item", {
+            "is-active": location.pathname === "/transmission/succeed",
+          })}
+        >
+          <SucceedIcon />
+          <span className="sub-tab-item-text">已完成</span>
+        </Link>
+      </>
+    );
+  };
+
+  const SyncSub = () => {
+    return (
+      <>
+        <SyncIcon className="sync-icon" />
+        <span className="sync-name">Laptop-wujunyi</span>
+        <span className="sync-system">linux</span>
+      </>
+    );
+  };
+
+  const ShowSubTab = () => {
+    if (location.pathname.startsWith("/transmission/")) {
+      return <TransmissionSub />;
+    } else if (location.pathname.startsWith("/sync/")) {
+      return <SyncSub />;
+    } else {
+      return <GroupSub />;
+    }
+  };
+
   return (
     <>
       <Sider width={78} theme="light">
         <div className="sider-tab">
           <div className="sider-tab-top">
-            <Logo className="logo" />
+            <LogoIcon className="logo" />
             <Link
               className={classnames("sider-tab-item", {
                 "is-active": location.pathname === "/home",
               })}
               to="/home"
             >
-              <Folder className="sider-tab-item-icon" />
+              <FolderIcon className="sider-tab-item-icon" />
               <span className="sider-tab-item-text">文件</span>
             </Link>
             <Link
@@ -64,7 +193,7 @@ const PageSider: FC = () => {
               })}
               to="/transmission"
             >
-              <Transmission className="sider-tab-item-icon" />
+              <TransmissionIcon className="sider-tab-item-icon" />
               <span className="sider-tab-item-text">传输</span>
             </Link>
             <Link
@@ -83,7 +212,7 @@ const PageSider: FC = () => {
                 className="sider-tab-setting"
                 size="large"
                 shape="circle"
-                icon={<Setting />}
+                icon={<SettingIcon />}
               />
             </Dropdown>
             <Link to="/userInfo">
@@ -97,7 +226,11 @@ const PageSider: FC = () => {
         </div>
       </Sider>
       <Sider width={168} theme="light">
-        <div className="sub-tab">sub-tab</div>
+        <div className="sub-tab">
+          <div className="sub-tab-content">
+            <ShowSubTab />
+          </div>
+        </div>
       </Sider>
     </>
   );
